5f3929c9c1e4b110304983f68b00bc22fd884365
Anjia Wang <anjiawang@gmail.com>
2020-09-20 23:47:17 -0400

    Add the manually transformed omp parallel testing file

---
+++ rex-runtime/transformed_omp_parallel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+
+void outlined_function() {
+	int id = omp_get_thread_num();
+	int num_threads = omp_get_num_threads();
+   	printf("Hello World from thread %d of %d!\n", id, num_threads);
+}
+
+int main(int argc, char* argv[])
+ {
+   __kmpc_fork_call(NULL, 0, outlined_function);
+
+   return 0;
+ }
+
